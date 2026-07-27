7f24a510e562730cf65e10df72e383d59202f913
lucifer <arifulhassanjoy02@gmail.com>
2026-07-27 23:38:34 +0600

    Add Regular Falsi method in Regular_Falsi.cpp
    Implement Regular Falsi method for root finding.

---
+++ Regular_Falsi.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+const double TOL = 1e-9;
+double func(double x)
+{
+    return {x * x * sqrt(x) - 5 * sqrt(x) - 19};
+}
+int main()
+{
+    double a, b, mid, res = 0;
+    cin >> a >> b;
+    if(func(a) * func(b) > 0)
+    {
+        cout << "NO root" << endl;
+        return 0;
+    }
+    else
+    {
+        mid = (a * func(b) - b * func(a)) / (func(b) - func(a));
+        while(abs(mid) > TOL)
+        {
+            mid = (a * func(b) - b * func(a)) / (func(b) - func(a));
+            if(func(mid) == 0) break;
+            if(func(a) * func(mid) < 0) b = mid;
+            else a = mid;
+            res = mid;
+            cout << "Mid: " << mid << endl;
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
