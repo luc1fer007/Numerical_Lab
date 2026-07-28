e18e02858bef8693a8c2e3d0b806d99ad4478268
lucifer <arifulhassanjoy02@gmail.com>
2026-07-28 06:52:09 +0600

    Refactor Bisection method function and output

--- Bisection_Method.cpp
+++ Bisection_Method.cpp
@@ -1,31 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
-double f(double x)
+const double TOL = 0.001;
+double func(double x)
 {
-    return {pow(x, 3) - x - 2};
+    return {x * x * x - 3 * sqrt(x) - 17};
 }
 int main()
 {
-    double a,b,mid;
-    int i = 0;
+    double a, b, mid;
     cin >> a >> b;
-    if(f(a) * f(b) > 0)
+    if(func(a) * func(b) > 0)
     {
-        cout << "No root available" << endl;
-        return 0;
+        cout << "No roots available!"<<endl; return 0;
     }
     else
     {
-        while((b - a) / 2 > 0.001)
+        int i = 0;
+        while((b - a) > TOL)
         {
             mid = (a + b) / 2;
-            if(f(mid)== 0)break;
-            else if(f(a) * f(mid) < 0)b = mid;
+            if(func(mid) == 0) break;
+            if(func(a) * func(mid) < 0) b = mid;
             else a = mid;
             i++;
-            cout << "Iteration: " << i << "|" << " 1st int: " << a << " | " << "2nd int: " << b <<" | Mid: " << (a+b) / 2 <<" | F(" << a << ") * " << "F(" << b << ") = " << f(a)*f(b) << endl;
+            cout << "Iteration: " << i << "|"
+                 << " 1st int: " << a << " | "
+                 << "2nd int: " << b <<" | Mid: "
+                 << (a+b) / 2.0 <<" | F(" << a << ") * "
+                 << "F(" << b << ") = " << func(mid) << endl;
         }
     }
-    cout << "The approximate root is : "<< mid << endl;
+    cout << "Root: " << (a + b) / 2.0 << endl;
     return 0;
 }
