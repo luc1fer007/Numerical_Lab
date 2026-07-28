5abbc05628c4e755146d9997fdfaace6f066e2bc
lucifer <arifulhassanjoy02@gmail.com>
2026-07-28 07:06:13 +0600

    Refactor Regular Falsi method implementation
    Fix function return statement and update loop condition for accuracy.

--- Regular_Falsi.cpp
+++ Regular_Falsi.cpp
@@ -1,14 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 const double TOL = 1e-9;
+
 double func(double x)
 {
-    return {x * x * sqrt(x) - 5 * sqrt(x) - 19};
+    return x * x * sqrt(x) - 5 * sqrt(x) - 19;
 }
+
 int main()
 {
     double a, b, mid, res = 0;
     cin >> a >> b;
+
     if(func(a) * func(b) > 0)
     {
         cout << "NO root" << endl;
@@ -17,15 +20,15 @@ int main()
     else
     {
         mid = (a * func(b) - b * func(a)) / (func(b) - func(a));
-        while(abs(mid) > TOL)
+        while(abs(func(mid)) > TOL)
         {
-            mid = (a * func(b) - b * func(a)) / (func(b) - func(a));
-            if(func(mid) == 0) break;
             if(func(a) * func(mid) < 0) b = mid;
             else a = mid;
+            mid = (a * func(b) - b * func(a)) / (func(b) - func(a));
             res = mid;
             cout << "Mid: " << mid << endl;
         }
+        res = mid;
         cout << res << endl;
     }
     return 0;
